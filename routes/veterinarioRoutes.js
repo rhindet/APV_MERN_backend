8d58525b3e033e07e253c03b3128d205d373fb57
rhindet <luisrcparra2@gmail.com>
2022-09-28 12:43:02 -0500

    first commit

---
+++ routes/veterinarioRoutes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {registrar,perfil,confirmar,
+        autenticar,olivdePassword,comprobarToken,
+        nuevoPassword,actualizarPerfil,actualizarPassword
+}from '../controllers/veterinarioController.js'
+
+import checkAuth  from "../middelware/authMiddelware.js";
+const router = express.Router();
+
+//area publica
+router.post('/',registrar);
+router.get('/confirmar/:token',confirmar);
+router.post('/login',autenticar)
+router.post('/olvide-password',olivdePassword)
+router.route("/olvide-password/:token").get(comprobarToken).post(nuevoPassword);
+
+//esto es igual a la sintaxis de arriba
+// router.get('/olvide-password/:token',comprobarToken)
+// router.post('/olvide-password/:token',nuevoPassword)
+
+//Area privada
+router.get('/perfil',checkAuth,perfil)
+router.put('/perfil/:id',checkAuth,actualizarPerfil)
+router.put('/actualizar-password',checkAuth,actualizarPassword)
+export default router;
